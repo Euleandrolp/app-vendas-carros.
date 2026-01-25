516ad254804a404f7a26061e7c98351bbc4fcd22
leandro <nandolimapr925@gmail.com>
2026-01-25 01:29:57 +0000

    app-vendas-carros

---
+++ cars/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from cars.models import Car
+
+
+def cars_view(request):
+    cars = Car.objects.all()
+    print(cars)
+
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars}
+    )
